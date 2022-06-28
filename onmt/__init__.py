a7fa5b012c2d70707febb1f75e0f6e95c29587dd
Joris Cadow <dow@zurich.ibm.com>
2022-06-28 19:55:25 +0200

    feat: bump version to 1.1.1.

--- onmt/__init__.py
+++ onmt/__init__.py
@@ -1,15 +1,17 @@
 """ Main entry point of the ONMT library """
 from __future__ import division, print_function
 
-import onmt.inputters
-import onmt.encoders
-import onmt.decoders
-import onmt.models
-import onmt.utils
-import onmt.modules
-from onmt.trainer import Trainer
 import sys
+
+import onmt.decoders
+import onmt.encoders
+import onmt.inputters
+import onmt.models
+import onmt.modules
+import onmt.utils
 import onmt.utils.optimizers
+from onmt.trainer import Trainer
+
 onmt.utils.optimizers.Optim = onmt.utils.optimizers.Optimizer
 sys.modules["onmt.Optim"] = onmt.utils.optimizers
 
@@ -17,4 +19,4 @@ sys.modules["onmt.Optim"] = onmt.utils.optimizers
 __all__ = [onmt.inputters, onmt.encoders, onmt.decoders, onmt.models,
            onmt.utils, onmt.modules, "Trainer"]
 
-__version__ = "1.1.0"
+__version__ = "1.1.1"
--- setup.py
+++ setup.py
@@ -1,28 +1,29 @@
 #!/usr/bin/env python
-from setuptools import setup, find_packages
 from os import path
 
+from setuptools import find_packages, setup
+
 this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
 setup(
-    name='rxn-opennmt-py',
-    description='Fork of OpenNMT-py for use in RXN projects',
+    name="rxn-opennmt-py",
+    description="Fork of OpenNMT-py for use in RXN projects",
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    version='1.1.0',
+    long_description_content_type="text/markdown",
+    version="1.1.1",
     packages=find_packages(),
     project_urls={
         "Documentation": "http://opennmt.net/OpenNMT-py/",
         "Forum": "http://forum.opennmt.net/",
         "Gitter": "https://gitter.im/OpenNMT/OpenNMT-py",
-        "Source": "https://github.com/OpenNMT/OpenNMT-py/"
+        "Source": "https://github.com/OpenNMT/OpenNMT-py/",
     },
     install_requires=[
         "six",
         "tqdm~=4.30.0",
-        "torch>=1.2,<1.5", # Originally torch==1.2.0, changed for compatibility with powerai-1.7.0 (pytorch-1.3.1)
+        "torch>=1.2,<1.4",  # Originally torch==1.2.0, changed for compatibility with powerai-1.7.0 (pytorch-1.3.1)
         "torchtext==0.4.0",
         "future",
         "configargparse",
@@ -34,5 +35,5 @@ setup(
             "onmt_translate=onmt.bin.translate:main",
             "onmt_preprocess=onmt.bin.preprocess:main",
         ],
-    }
+    },
 )
