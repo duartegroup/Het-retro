40170b4a793537744691c5eb3777750d7f006467
ewawieczorek <ewa.wieczorek@gtc.ox.ac.uk>
2024-04-19 11:32:42 +0100

    Rename dataset files

--- data/uspto_dataset/merge_reactant_splits.py
+++ data/uspto_dataset/merge_reactant_splits.py
@@ -1,9 +1,9 @@
 lines = []
-with open ('src-train-split-1.txt', 'r') as f:
+with open ('reactant-train-split-1.txt', 'r') as f:
     for line in f.readlines():
         lines.append(line)
-with open ('src-train-split-2.txt', 'r') as f:
+with open ('reactant-train-split-2.txt', 'r') as f:
     for line in f.readlines():
         lines.append(line)
-with open('src-train.txt', 'w') as f:
+with open('reactant-train.txt', 'w') as f:
     f.write(''.join(lines))
